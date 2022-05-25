8f26ef7558ef7df2664a2783cdf00401fc0fac20
Chris Porter <cap8095@gmail.com>
2022-05-24 20:34:39 -0400

    getting the models set up

---
+++ models/index.js
@@ -0,0 +1,34 @@
+// import models
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+const seedProducts = require("../seeds/product-seeds");
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "SET NULL",
+});
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+});
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+});
+// Products belongToMany Tags (through ProductTag)
+//Belongs to, hasMany, Belong to many are methods from sequelize
+// Tags belongToMany Products (through ProductTag)
+//any time that there's a many to many relationship you must define the "through:" which
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
